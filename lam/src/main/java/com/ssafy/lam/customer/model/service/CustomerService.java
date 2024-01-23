c449f6e60bf98ca8acb4ce6dc932caa1dd00a841
George-Polya <wlskaka4@gmail.com>
2024-01-23 10:07:04 +0900

    fix: 구조 수정, Service추가

--- lam/src/main/java/com/ssafy/lam/customer/model/service/CustomerService.java
+++
@@ -1,9 +0,0 @@
-package com.ssafy.lam.customer.model.service;
-
-import com.ssafy.lam.dto.Customer;
-
-import java.util.List;
-
-public interface CustomerService {
-    List<Customer> findAll();
-}
--- src/main/java/com/ssafy/lam/dto/Customer.java
+++ src/main/java/com/ssafy/lam/dto/Customer.java
@@ -1,8 +1,16 @@
 package com.ssafy.lam.dto;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Customer {
 
     @Id
@@ -10,26 +18,5 @@ public class Customer {
     private int id;
     private String name;
 
-    Customer(int id, String name) {
-        this.id = id;
-        this.name = name;
-    }
 
-    public Customer() {}
-
-    public int getId() {
-        return id;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
 }
